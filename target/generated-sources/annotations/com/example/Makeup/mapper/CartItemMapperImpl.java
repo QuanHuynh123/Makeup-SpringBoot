30c48a81ab56fdc657ed651e58ffebbdab835e4b
QuanHuynh123 <huynhminhquan07072002@gmail.com>
2024-11-10 16:55:54 +0700

    update service entity

---
+++ target/generated-sources/annotations/com/example/Makeup/mapper/CartItemMapperImpl.java
@@ -0,0 +1,99 @@
+package com.example.Makeup.mapper;
+
+import com.example.Makeup.dto.CartItemDTO;
+import com.example.Makeup.entity.Cart;
+import com.example.Makeup.entity.CartItem;
+import com.example.Makeup.entity.Product;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-11-02T16:19:48+0700",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.3 (Oracle Corporation)"
+)
+@Component
+public class CartItemMapperImpl implements CartItemMapper {
+
+    @Override
+    public CartItemDTO toCartItemDTO(CartItem cartItem) {
+        if ( cartItem == null ) {
+            return null;
+        }
+
+        CartItemDTO cartItemDTO = new CartItemDTO();
+
+        cartItemDTO.setCartId( cartItemCartId( cartItem ) );
+        cartItemDTO.setProductId( cartItemProductId( cartItem ) );
+        cartItemDTO.setId( cartItem.getId() );
+        cartItemDTO.setQuantity( cartItem.getQuantity() );
+        cartItemDTO.setPrice( cartItem.getPrice() );
+
+        return cartItemDTO;
+    }
+
+    @Override
+    public CartItem toCartItemEntity(CartItemDTO cartItemDTO) {
+        if ( cartItemDTO == null ) {
+            return null;
+        }
+
+        CartItem cartItem = new CartItem();
+
+        cartItem.setCart( cartItemDTOToCart( cartItemDTO ) );
+        cartItem.setProduct( cartItemDTOToProduct( cartItemDTO ) );
+        cartItem.setId( cartItemDTO.getId() );
+        cartItem.setQuantity( cartItemDTO.getQuantity() );
+        cartItem.setPrice( cartItemDTO.getPrice() );
+
+        return cartItem;
+    }
+
+    private int cartItemCartId(CartItem cartItem) {
+        if ( cartItem == null ) {
+            return 0;
+        }
+        Cart cart = cartItem.getCart();
+        if ( cart == null ) {
+            return 0;
+        }
+        int id = cart.getId();
+        return id;
+    }
+
+    private int cartItemProductId(CartItem cartItem) {
+        if ( cartItem == null ) {
+            return 0;
+        }
+        Product product = cartItem.getProduct();
+        if ( product == null ) {
+            return 0;
+        }
+        int id = product.getId();
+        return id;
+    }
+
+    protected Cart cartItemDTOToCart(CartItemDTO cartItemDTO) {
+        if ( cartItemDTO == null ) {
+            return null;
+        }
+
+        Cart cart = new Cart();
+
+        cart.setId( cartItemDTO.getCartId() );
+
+        return cart;
+    }
+
+    protected Product cartItemDTOToProduct(CartItemDTO cartItemDTO) {
+        if ( cartItemDTO == null ) {
+            return null;
+        }
+
+        Product product = new Product();
+
+        product.setId( cartItemDTO.getProductId() );
+
+        return product;
+    }
+}
---
+++ target/generated-sources/annotations/com/example/Makeup/mapper/ServiceMakeupMapperImpl.java
@@ -0,0 +1,47 @@
+package com.example.Makeup.mapper;
+
+import com.example.Makeup.dto.ServiceMakeupDTO;
+import com.example.Makeup.entity.ServiceMakeup;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-11-02T16:19:48+0700",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.3 (Oracle Corporation)"
+)
+@Component
+public class ServiceMakeupMapperImpl implements ServiceMakeupMapper {
+
+    @Override
+    public ServiceMakeupDTO toServiceMakeupDTO(ServiceMakeup serviceMakeup) {
+        if ( serviceMakeup == null ) {
+            return null;
+        }
+
+        ServiceMakeupDTO serviceMakeupDTO = new ServiceMakeupDTO();
+
+        serviceMakeupDTO.setId( serviceMakeup.getId() );
+        serviceMakeupDTO.setNameService( serviceMakeup.getNameService() );
+        serviceMakeupDTO.setDescription( serviceMakeup.getDescription() );
+        serviceMakeupDTO.setPrice( serviceMakeup.getPrice() );
+
+        return serviceMakeupDTO;
+    }
+
+    @Override
+    public ServiceMakeup toServiceMakeupEntity(ServiceMakeupDTO serviceMakeupDTO) {
+        if ( serviceMakeupDTO == null ) {
+            return null;
+        }
+
+        ServiceMakeup serviceMakeup = new ServiceMakeup();
+
+        serviceMakeup.setId( serviceMakeupDTO.getId() );
+        serviceMakeup.setNameService( serviceMakeupDTO.getNameService() );
+        serviceMakeup.setDescription( serviceMakeupDTO.getDescription() );
+        serviceMakeup.setPrice( serviceMakeupDTO.getPrice() );
+
+        return serviceMakeup;
+    }
+}
