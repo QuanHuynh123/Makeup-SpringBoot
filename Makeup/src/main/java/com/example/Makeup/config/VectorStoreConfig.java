e001177572b6d7b06a9f524b1f27b2234694c412
QuanHuynh123 <huynhminhquan07072002@gmail.com>
2025-08-05 23:02:06 +0700

    test spring ai

---
+++ Makeup/src/main/java/com/example/Makeup/config/VectorStoreConfig.java
@@ -0,0 +1,13 @@
+package com.example.Makeup.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class VectorStoreConfig {
+
+//    @Bean
+//    public VectorStore redisVectorStore(RedisTemplate<String, byte[]> redisTemplate) {
+//        return new RedisVectorStore(redisTemplate);
+//    }
+}
