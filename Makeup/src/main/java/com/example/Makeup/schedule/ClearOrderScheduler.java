99852fcc30533bc21fa63beb99243f2f3763e0be
QuanHuynh123 <huynhminhquan07072002@gmail.com>
2025-08-05 23:01:34 +0700

    add cron job clear order pay expired

---
+++ Makeup/src/main/java/com/example/Makeup/schedule/ClearOrderScheduler.java
@@ -0,0 +1,20 @@
+package com.example.Makeup.schedule;
+
+import com.example.Makeup.service.IOrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ClearOrderScheduler {
+
+    private final IOrderService orderService;
+
+
+    @Scheduled(cron = "0 0 0 */3 * ?")
+    public void clearOrder() {
+        orderService.clearOrderPaymentExpired();
+    }
+
+}
