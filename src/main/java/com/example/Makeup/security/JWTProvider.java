4654fedbefbd546ea84046d22b0ec7cc1347e9e6
QuanHuynh123 <huynhminhquan07072002@gmail.com>
2026-03-04 21:45:21 +0700

    fix value secret key

--- src/main/java/com/example/Makeup/security/JWTProvider.java
+++ src/main/java/com/example/Makeup/security/JWTProvider.java
@@ -20,7 +20,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class JWTProvider {
 
-  @Value("${jwt.secret}")
+  @Value("${JWT_SECRET}")
   private String secretkey;
 
   @Value("${jwt.expiration}")
