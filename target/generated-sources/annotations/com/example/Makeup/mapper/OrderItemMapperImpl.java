48cc7815f3fb98097d5507235b070387271583e1
QuanHuynh123 <huynhminhquan07072002@gmail.com>
2026-04-01 21:21:49 +0700

    update config redis

--- target/generated-sources/annotations/com/example/Makeup/mapper/OrderItemMapperImpl.java
+++ target/generated-sources/annotations/com/example/Makeup/mapper/OrderItemMapperImpl.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-03-21T00:11:26+0700",
+    date = "2026-04-01T21:04:50+0700",
     comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.45.0.v20260224-0835, environment: Java 21.0.10 (Eclipse Adoptium)"
 )
 @Component
